fdf7b9707a8224fd59d30f25993f19b5933d25a7
Hazel Bamba <hazel.bamba02@gmail.com>
2020-06-01 14:46:48 +1000

    First commit to the project

---
+++ Dusts/ExampleSolution.cs
@@ -0,0 +1,11 @@
+using Terraria.ModLoader;
+
+namespace hazymod.Dusts
+{
+	public class ExampleSolution : ModDust
+	{
+		public override void SetDefaults() {
+			updateType = 110;
+		}
+	}
+}
---
+++ Items/whoop.cs
@@ -0,0 +1,39 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace hazymod.Items
+{
+	public class whoop : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			// DisplayName.SetDefault("whoop"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("This is a basic modded sword.");
+		}
+
+		public override void SetDefaults() 
+		{
+			item.damage = 15000;
+			item.melee = true;
+			item.width = 1000;
+			item.height = 1000;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = 1;
+			item.knockBack = 6;
+			item.value = 10000;
+			item.rare = 2;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
+
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.DirtBlock, 10);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
